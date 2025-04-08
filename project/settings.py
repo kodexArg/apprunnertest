fcaab56a167765359452071d4f7ce639ae4b00ff
kodexArg <gcavedal@gmail.com>
2025-04-08 17:46:57 -0300

    checkpoint

--- project/settings.py
+++ project/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- startup.sh
+++ startup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
-gunicorn --workers 2 project.wsgi:application --bind 0.0.0.0:$PORT
+python3 manage.py migrate --noinput && python3 manage.py collectstatic --noinput && gunicorn --workers 2 project.wsgi:application --bind 0.0.0.0:8080
