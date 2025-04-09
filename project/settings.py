843feb9d1fa95bae55ac1ac64bcee17091faa50d
kodexArg <gcavedal@gmail.com>
2025-04-09 12:29:12 -0300

    checkpoint

--- project/settings.py
+++ project/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() in ('true', '1', 't')
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',') if os.getenv('CSRF_TRUSTED_ORIGINS') else []
 
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
@@ -47,7 +47,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('WELPDESK_DB_CONNECTOR'),
+        default=os.getenv('DATABASE_URL', os.getenv('WELPDESK_DB_CONNECTOR')),
         conn_max_age=600,
         conn_health_checks=True,
         ssl_require=True
