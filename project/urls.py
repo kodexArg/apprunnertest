b7b4a35da2408f0d3f18018ce9a1b66bfbacad1a
kodexArg <gcavedal@gmail.com>
2025-04-09 12:24:06 -0300

    checkpoint

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
-from core.views import HelloWorldView, DatabaseInfoView, PingView
+from core.views import HelloWorldView, DatabaseInfoView, PingView, DbView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
