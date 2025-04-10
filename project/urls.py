86d0a670dfca3735138b4870c903435c2310efda
kodexArg <gcavedal@gmail.com>
2025-04-10 14:17:38 -0300

    checkpoint

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from core.views import HelloWorldView, PingView, DbView, DatabaseInfoView
+from django.http import HttpResponse
+
+def health_check(request):
+  return HttpResponse("OK", status=200)
+
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', HelloWorldView.as_view(), name='hello_world'),
     path('ping/', PingView.as_view(), name='ping'),
     path('db/', DbView.as_view(), name='db'),
     path('db-info/', DatabaseInfoView.as_view(), name='db_info'),
+    path('healthz/', health_check, name='health_check'),
 ]
