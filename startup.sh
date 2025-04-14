8c9b49f5ab8bb63991db8f1b0ec4199dfc56dc91
gcavedal <gcavedal@gmail.com>
2025-04-14 15:07:45 +0000

    checkpoint

--- startup.sh
+++ startup.sh
@@ -4,13 +4,12 @@ set -e
 
 echo ">>> [Startup Script] Iniciando script..."
 
-echo ">>> [Startup Script] Aplicando migraciones de base de datos y collectstatic..."
-pipenv run python manage.py migrate --noinput
-pipenv run python manage.py collectstatic --noinput
+echo ">>> [Startup Script] Aplicando migraciones de base de datos..."
+python manage.py migrate --noinput
 echo ">>> [Startup Script] Migraciones aplicadas."
 
 echo ">>> [Startup Script] Iniciando Gunicorn..."
-exec pipenv run gunicorn project.wsgi:application \
+exec gunicorn project.wsgi:application \
     --bind 0.0.0.0:$PORT \
     --log-level debug \
     -t 240 \
