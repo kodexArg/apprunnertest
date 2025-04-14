62813f8dd3a561412d674cbe97d9c4cea5b88435
kodexArg <gcavedal@gmail.com>
2025-04-14 17:39:57 -0300

    checkpoint

--- startup.sh
+++ startup.sh
@@ -15,4 +15,4 @@ pipenv run python3 manage.py collectstatic --noinput
 pipenv run python3 manage.py migrate
 
 # Start the Gunicorn server
-pipenv run gunicorn3 cakes_project.wsgi:application --bind 0.0.0.0:8000 --workers 2 --log-file -
+pipenv run gunicorn3 project.wsgi:application --bind 0.0.0.0:8000 --workers 2 --log-file -
