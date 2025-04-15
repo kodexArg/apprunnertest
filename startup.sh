a9e89222f7ffd49604487148d850226854560f1a
kodexArg <gcavedal@gmail.com>
2025-04-15 15:28:44 -0300

    checkpoint

--- startup.sh
+++ startup.sh
@@ -1,19 +1,11 @@
-#!/bin/bash
-
-# Install dependencies
-pip3 install pipenv
-pipenv install --system --deploy --ignore-pipfile
-
-# print virtualenv, pipenv, freeze
-echo "Virtualenv: $(which python3)"
-echo "Pipenv: $(pipenv --version)"
-echo "Freeze: $(pipenv freeze)"
+#!/bin/sh
+set -e
 
 # Collect static files
-pipenv run python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
-pipenv run python3 manage.py migrate
+python3 manage.py migrate
 
 # Start the Gunicorn server
-pipenv run gunicorn3 project.wsgi:application --bind 0.0.0.0:8000 --workers 2 --log-file -
+gunicorn project.wsgi:application --bind 0.0.0.0:${PORT:-8080} --workers 3 --log-file -
